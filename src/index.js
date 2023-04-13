1760fc5978d74c5c2ff81c83e088a349a2477127
Dmytro <103310889+Dmytro-Hlushchenko@users.noreply.github.com>
2023-04-13 17:58:36 +0300

    add-reduce-html

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-//Якщо користувач повністю очищає поле пошуку, то HTTP-запит не виконується, а розмітка списку країн або інформації про країну зникає.
 //Виконай санітизацію введеного рядка методом trim(), це вирішить проблему, коли в полі введення тільки пробіли, або вони є на початку і
 // в кінці рядка.
 import  debounce  from 'lodash.debounce';
@@ -11,26 +10,39 @@ const countryNameInput = document.querySelector('#search-box');
 const countryListEl = document.querySelector('.country-list');
 const countryInfoEl = document.querySelector('.country-info');
 const result = fetchCountries();
-countryNameInput.addEventListener('input', debounce(onCoutryNameInput, DEBOUNCE_DELAY));
+countryNameInput.addEventListener('input', debounce(onCountryNameInput, DEBOUNCE_DELAY));
 
-function onCoutryNameInput(e) {
+function onCountryNameInput(e) {
      e.preventDefault();
     const country = countryNameInput.value.trim();
     if (country.length === 0) {
         console.log('No DATA');
-        return;
+        countryListEl.innerHTML = '';
+
     }
     fetchCountries(country)
-        .then((countries) => console.log(countries))
-         
+        .then((countries) => createGallery(countries))
+        .catch(onErrore);
+    
+        function createGallery(items) {
+            const list = items.reduce((markup, item) => markup + createCountryCard(item), "");
+            countryListEl.innerHTML = list;
+     }; 
+    
+    function createCountryCard({flags, name, capital, population, languages}) {
+        console.log(flags.svg)
+        return `<li>
+        <img src = "${flags.svg}" width = 30 hiegth = 30></img>
+        <p>Country: ${name.official}</p>
+        <p>Capital: ${capital}</p>
+        <p>Population: ${population}</p>
+        
+        <p>Languages: ${Object.values(languages)}</p>
+        </l/i>`
+    };
+
+        function onErrore(err) { 
+            console.error(err);
+        }
+    
 } ;
-
-
-//  function createGallery(items) {
-//     return items.map(({ preview, original, description }) => {
-//         return `<li class="gallery__item">
-//                      <img class="gallery__image" src="${preview}" alt="${description}"/>
-//                      <p class = "item_field">${}</p>                      
-//                 </li>`;
-//     }).join('');
-// }; 
